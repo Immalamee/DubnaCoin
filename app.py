2254c77fe3741a2a1426892ed6d347bbb648fd70
Immalamee <moldabekuzbekov@gmail.com>
2024-11-01 18:21:48 +0400

    aa

--- app.py
+++ app.py
@@ -365,4 +365,4 @@ if __name__ == '__main__':
         conn.close()
 
     start_autoclicker()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
