e0cab296202a32ca89b9d78af1053c389f2d2212
Immalamee <moldabekuzbekov@gmail.com>
2024-11-07 20:59:59 +0400

    Окак

--- app.py
+++ app.py
@@ -385,6 +385,6 @@ if __name__ == '__main__':
     start_autoclicker()
     try:
         print("Запуск приложения...")
-        app.run(host='0.0.0.0', port=8000, debug = True)
+        app.run(host='0.0.0.0', port=5000, debug = True)
     except Exception as e:
         print(f"Ошибка при запуске приложения: {e}")
